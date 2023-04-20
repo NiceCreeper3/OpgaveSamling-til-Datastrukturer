b43730515121672b99bda114df7b2fc2f7e04a75
NiceCreeper3 <124564568+NiceCreeper3@users.noreply.github.com>
2023-04-20 15:00:53 +0200

    Somthing

--- DataStrugOp3/StringLibrary/Class1.cs
+++ DataStrugOp3/StringLibrary/Class1.cs
@@ -2,6 +2,17 @@
 
 namespace StringLibrary
 {
+    public static class StringLibrary
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
     public class Class1
     {
     }
